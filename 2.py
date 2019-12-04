d54743daf731eb80631cc94481d834df030ad83c
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-12-04 14:44:16 +0100

    Added named collections and dataclass parameters

---
+++ 2.py
@@ -0,0 +1,113 @@
+import random
+from dataclasses import dataclass
+
+# print(dir(collections))
+
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+# dataclass powoduje ze nie musimy miec inicjalizatora ale musimy podac typ i mozemy dodatkowo wart.domyslna
+# dodatkowo ma __repr__ wbudowane wiec ladnie wyprintowuje obiekt
+# porownywanie 5 operacji
+
+@dataclass(order=True)
+class Number:
+    value:int=4
+
+one = Number(1)
+print(one.value)
+print(one)
+
+
+class NumberOrd:
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return str(self.value)
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+one = NumberOrd(1)
+two = NumberOrd(2)
+print('one == one')
+print(one == one)
+print('one < two')
+print(one < two)
+
+
+num1 = Number(1)
+num2 = Number(2)
+
+print('dataclass number 1 < 2')
+print(num1<num2)
+
+# porownanie oobiektow __gt__ __ge__ __le__ __lt__ __ne__ __eq__
+# <, >, == <= >= !=
+
+class A:
+    pass
+
+# print(dir(A))
+
+a1 = A()
+a2 = A()
+# to nie wyjdzie
+print('a1 A() == a2 A()')
+print(a1 == a2)
+
+
+@dataclass(order=True)
+class Person:
+    name: str
+    age: int = 0
+
+    def __eq__(self, other):
+        return (self.name, self.age) == (other.name, other.age)
+
+
+a = [NumberOrd(random.randint(1,10)) for i in range(10)]
+
+print(a)
+result = []
+for i in range(1,11):
+    result.append(i**2)
+print(result)
+
+# result = [i*2 for i in a]
+print(result)
+
+
+a = [NumberOrd(random.randint(1,10)) for _ in range(10)]
+print(a)
+sorted_a = sorted(a)
+print(sorted_a)
+reversed_sorted_a = sorted(a, reverse=True)
+print(reversed_sorted_a)
+
+@dataclass(init=True, repr=True, eq=False, order=False, frozen=False)
+class C:
+    pass
+
+@dataclass(eq=False, repr = False)
+class Number_c:
+    value: int
+
+a = Number_c(1)
+print(a)
+b = Number_c(2)
+c = Number_c(3)
+print(a==b)
+
+@dataclass(frozen=True)
+class Number_d:
+    value: int=0
+
+d = Number_d(5)
+print(d)
+# d.val ue = 3
--- namedcollections_1.py
+++ namedcollections_1.py
@@ -1,8 +1,11 @@
+import math
+
+print(dir(math))
+
 import collections
 
 jack = ("Jack", 30, "male")
 print(f"Tuple representation {jack}")
-
 
 Person = collections.namedtuple('Person', 'name age gender')
 # generuje klase z polami nazwanymi
@@ -13,7 +16,7 @@ print(f"Tuple representation {jack}")
 
 print(f'Field by name: {jack.name}')
 
-jane = Person(name="jane",age=29,gender="female")
+jane = Person(name="jane", age=29, gender="female")
 print(jane)
 print(f'Field by age: {jane.age}')
 
@@ -22,3 +25,4 @@ num1 = Number(number='343678234', area_code="+48", delimiter="-")
 num2 = Number(number='999998233', area_code="+44", delimiter="-")
 num3 = Number(number='121879311', area_code="+42", delimiter="-")
 print(f'{num1} {num2} {num3}')
+print(f'nr telefonu: {num1.area_code}{num1.delimiter}{num1.number}')
