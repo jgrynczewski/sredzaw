491ba09d0f134afdbb1ca36f5dbc2aeee639221e
stefan <tom.s.mal@o2.pl>
2019-12-04 12:40:27 +0100

    drugi phonebook

--- 1.py
+++ 1.py
@@ -14,3 +14,9 @@ jane = Person(name="Jane", age=29, gender="female")
 print(f'Field by age : {jane.age}')
 
 
+print('----------------')
+Phonebook = collections.namedtuple('Phonebook', 'number area_code, delim')
+adam = Phonebook(number=555444333, area_code="+48", delim="-")
+print(f'field by number: {adam.number}')
+
+
