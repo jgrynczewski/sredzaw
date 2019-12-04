290c62f616218ed05b82f605a7c941debbfa7ff4
kataryna <kzurawska@gmail.com>
2019-12-04 12:40:08 +0100

    namedtuples

--- 1.py
+++ 1.py
@@ -1,10 +1,13 @@
 import collections
+import math
+from math import *
 
 jack = ("Jack", 30, "male")
 Person = collections.namedtuple("Person", "name age gender") #funkcja namedtuple zwraca nam klasę Person
 print(f'Type of Person: {type(Person)}')
 
 jack = Person("Jack", 30, 'male')
+
 print(f" Namedtuple representation {jack}")
 print("Field by name", jack.name)
 
