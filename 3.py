7549cce6e7773d19537336deef95684ccc0e774a
maciej <anbia66@gmail.com>
2019-12-04 21:09:51 +0100

    dodano klase z użyciem wyjątku liczącą polę koła

--- 3.py
+++ 3.py
@@ -11,6 +11,33 @@ try:
 except ValueError:
     print('Nieprawidłowe dane')
 
+print('--------------as a class:------------------')
+
+
+class ObliczPoleKola:
+    def __init__(self):
+        self.radius = input('podaj promień koła')
+        self.result = 0
+
+    def validate(self):
+        try:
+            number = float(self.radius)
+            return number
+        except ValueError:
+            return False
+
+    def calculate(self):
+        if not self.validate():
+            return 'Podano nieprawidłową wartość'
+        else:
+            r = self.validate()
+            result = pi * r**2
+            return f'Pole koła o promieniu {r} wynosi {round(result, 2)}'
+
+walidacja = ObliczPoleKola().validate()
+print(f'Walidacja: {walidacja}')
+oblicz_pole1 = ObliczPoleKola().calculate()
+print(f'Wynik: {oblicz_pole1}')
 
 
 
