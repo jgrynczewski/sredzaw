20a0663277cf707ed76d9e44a5b277ec0ddf4623
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-12-04 12:22:38 +0100

    Added named tuple 1.py

---
+++ 1.py
@@ -0,0 +1,24 @@
+import collections
+
+jack = ("Jack", 30, "male")
+print(f"Tuple representation {jack}")
+
+
+Person = collections.namedtuple('Person', 'name age gender')
+# generuje klase z polami nazwanymi
+print(f"Type of Person: {type(Person)}")
+
+jack = Person(name="Jack", age=30, gender="male")
+print(f"Tuple representation {jack}")
+
+print(f'Field by name: {jack.name}')
+
+jane = Person(name="jane",age=29,gender="female")
+print(jane)
+print(f'Field by age: {jane.age}')
+
+Number = collections.namedtuple('Number', 'number area_code delimiter')
+num1 = Number(number='343678234', area_code="+48", delimiter="-")
+num2 = Number(number='999998233', area_code="+44", delimiter="-")
+num3 = Number(number='121879311', area_code="+42", delimiter="-")
+print(f'{num1} {num2} {num3}')
