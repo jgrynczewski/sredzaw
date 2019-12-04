fead6e3bcb9cd3f550d84008961aad2320fac4f5
Jerzy <jgrynczewski@gmail.com>
2019-12-04 14:44:00 +0100

    dataclasses part2

--- 2.py
+++ 2.py
@@ -28,3 +28,40 @@ class Number_b:
 num = Number_b(5)
 num2 = Number_b(2)
 print(num > num2)
+
+@dataclass(order=True)
+class Person:
+    name : str
+    age: int = 0
+
+import random
+
+a = [Number_b(random.randint(1,10)) for _ in range(10)]
+print(a)
+sorted_a = sorted(a)
+print(sorted_a)
+reversed_sorted_a = sorted(a, reverse=True)
+print(reversed_sorted_a)
+
+@dataclass(init=True, repr=True, eq=False, order=False, frozen=False)
+class C:
+    pass
+
+@dataclass(repr = False, eq=True)
+class Number_c:
+    value: int
+
+a = Number_c(1)
+print(a)
+b = Number_c(2)
+b.value = 1
+c = Number_c(3)
+print(a==b)
+
+@dataclass(frozen=True)
+class Number_d:
+    value: int = 0
+
+a = Number_d(1)
+print(a.value)
+a.value = 2
