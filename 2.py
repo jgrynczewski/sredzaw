7d96c543eb442fc7c84016915efb01058e0b59ff
maciej <anbia66@gmail.com>
2019-12-04 15:16:42 +0100

    dataclass part 2

--- 2.py
+++ 2.py
@@ -33,11 +33,91 @@ print(num.value)
 print(num > num2)
 print('---------------------------------------------------')
 
-class A:
+@dataclass(order=True)
+class Person:
+    name: str
+    age: int = 0
+
+    def __eq__(self, other): #porownanie krotek
+        return (self.name, self.age) == (other.name, other.age)
+
+@dataclass(order=True) #automatycznie
+class Person:
+    name: str
+    age: int = 0
+
+ #list comprehension
+import random
+a = [Number_b(random.randint(1,10)) for _ in range(10)] # _ - znaczy ze to tymczasowe(konwencja)
+print (a)
+result = []
+for i in range(1, 5):
+    result.append(i**2)
+
+print (result)
+
+result = [i**2 for i in range(1, 5)]
+
+print (result)
+
+#sorted()
+a = [Number_b(random.randint(1,10)) for _ in range(10)] # _ - znaczy ze to tymczasowe(konwencja)
+print (a)
+sorted_a = sorted(a)
+print(sorted_a)
+reversed_sorted_a = sorted(a, reverse=True)
+print(reversed_sorted_a)
+
+@dataclass(init=True, repr=True, eq=False, order=False, frozen=False)
+class C:
     pass
-a1 = A()
-a2 = A()
-print(a1 == a2)#blad not supported - tzn. ze klasa A nie ma metody __gt__
+
+@dataclass(repr=False, eq=True)
+class Number_c:
+    value: int
+    
+a = Number_c(2)
+c = Number_c(3)
+print(a == c)
+
+@dataclass(frozen=False)  #jak True nie mozna potem zmienic wartosci, wywala blad
+class Number_d:
+    value: int = 0
+
+a = Number_d(1)
+print(a.value)
+a.value = 2
+print(a.value)
+
+import math
+'''
+class Float:
+    def __init__(self, value=0):
+        self.value = value
+        self.process()
+
+    def process(self):
+        self.decimal, self.integer = math.modf(self.value)
+        #rozpakowywanie kontenerów
+
+a = Float(1, 1)
+print (a.value) '''
+@dataclass   #dziedziczenie w dataclass
+class Person:
+    name: str
+    age: int = 0
+@dataclass
+class Student(Person):
+    grade: int = 0
+
+s = Student(20, 'John', 2)
+print(s.name, s.age, s.grade)
+
+#@dataclass
+#class B(A)
+
+
+
 
 
 
