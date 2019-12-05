2b079ea301dd19376c31c71d95c7e572453a3ede
Magda <magda.bochinska@gmail.com>
2019-12-05 12:56:35 +0100

    rozpakowywanie cz. dalsza

--- args_kwargs.py
+++ args_kwargs.py
@@ -19,10 +19,29 @@ print(a)
 t = (43, 4, 5, 6)
 print(*t)
 
-a, b, *c = (1,2,3,4,5,6,7)
+*a, b, c = (1,2,3,4,5,6,7)
 print(a)
 print(b)
 print(c)
 
 d={'a':7, 'b':4}
 print(*d)
+
+
+def func(a, b, c, d):
+    print(f'{a} {b} {c} {d}')
+
+func(*t)
+
+def func(**kwargs):
+    print(kwargs)
+
+func(**d)
+
+l = [1,53,2,5]
+d = {'reverse': True}
+print(sorted(l, reverse = True))
+print(sorted(l, **d))
+
+d1 = {'sep':'aa'}
+print('asd', 'qwe', **d1)
