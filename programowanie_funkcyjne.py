ba7d28b0ec3345304f8cad0928f647eadca99f0d
Magda <magda.bochinska@gmail.com>
2019-12-05 15:51:16 +0100

    lambda

--- programowanie_funkcyjne.py
+++ programowanie_funkcyjne.py
@@ -38,3 +38,31 @@ def f3(x):
 print(list(filter(f3,range(1,11))))
 
 #reduce
+
+from functools import reduce
+
+print(reduce(lambda x,y : 3*x+y, [1,2,3]))
+
+print(reduce(lambda x,y : 3*x+y, ['Jan Kowalski', '']))
+
+# silnia
+
+print(reduce(lambda x,y: x*y, range(1,5)))
+
+def func_pomocnicza(x):
+    return x[1]
+
+def func_pomocnicza_2(x):
+    return x[0]**2+x[1]**2
+
+pairs = [[1, 18], [2, 9], [3, 8]]
+print(min(pairs))
+print(max(pairs))
+
+print(min(pairs, key = func_pomocnicza))
+print(max(pairs, key = func_pomocnicza))
+
+print(min(pairs, key = lambda x: x[1]))
+print(max(pairs, key = lambda x: x[0]**2 + x[1]**2))
+
+
