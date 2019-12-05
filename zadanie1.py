20911b433a7693d8857308b223cca58fcd16d0ae
Jerzy <jgrynczewski@gmail.com>
2019-12-05 09:42:23 +0100

    Zadanie1 - rozwiazanie

--- zadanie1.py
+++ zadanie1.py
@@ -4,3 +4,27 @@
 # Utwórz kilka przykładów samochodu używając namedtuple Car
 
 # Powtórz to samo używając dataclass
+
+from collections import namedtuple
+
+Car = namedtuple("Car", "marka model liczba_drzwi")
+
+my_car_1 = Car("BMV", "V6", 5)
+my_car_2 = Car("Volkswagen", "Up", 3)
+
+print(my_car_1)
+print(my_car_2.liczba_drzwi)
+
+from dataclasses import dataclass
+
+@dataclass
+class Car:
+    marka: str
+    model: str
+    liczba_drzwi: int
+
+my_car_1 = Car("BMV", "V6", 5)
+my_car_2 = Car("Volkswagen", "Up", 3)
+
+print(my_car_1)
+print(my_car_2.liczba_drzwi)
