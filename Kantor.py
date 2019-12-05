aba0a38df4d0a6918f1a2025dda48db61c9b51c2
kataryna <kzurawska@gmail.com>
2019-12-05 10:57:59 +0100

    kantor

--- Kantor.py
+++ Kantor.py
@@ -2,7 +2,8 @@
 Zadanie 21.
 Utwórz dwie namedtuple:
 - 'Amount' z dwoma polami: kwota oraz skrót nazwy waluty
-- 'Currnency' z trzema polami: nazwa skrót nazwy oraz przelicznik na PLN2. Utwórz cztery instsancje Currency:
+- 'Currency' z trzema polami: nazwa skrót nazwy oraz przelicznik na PLN2.
+Utwórz cztery instancje Currency:
 - EUR (z kursem 4.32)
 - USD (z kursem 3.93)
 - CHF (z kursem 3.95)
@@ -28,38 +29,73 @@ from collections import namedtuple
 from dataclasses import dataclass
 
 
-Amount = namedtuple(("Amount","kwota nazwa_waluty"))
-Currency = namedtuple(("Currency","nazwa skrot_nazwy przelicznik"))
-eur = Currency('Euro','EUR',4.32)
-usd = Currency('Dolar','USD',3.93)
-chf = Currency('Frank','CHF',3.95)
-pln = Currency('Złoty','PLN',1.00)
+Amount = namedtuple("Amount","kwota skrot")
+a1 = Amount(30, 'PLN')
+a2 = Amount(10, 'CHF')
 
+Currency = namedtuple("Currency","nazwa skrot przelicznik")
+
+c1 = Currency('euro','EUR',4.32)
+c2 = Currency('dolar','USD',3.93)
+c3 = Currency('frank','CHF',3.95)
+c4 = Currency('złoty','PLN',1.00)
+print(c3)
 
 @dataclass
 class CurrencyAccount:
-    skrot_waluty: str
-    stan_konta: int
-    wartosc_debetu: int
+    skrot: str
+    stan: float
+    debet: float
 
     @property
     def total_available(self):
-        return  self.stan_konta + self.wartosc_debetu
+        return  Amount(self.stan + self.debet, self.skrot)
 
     def pay_out(self, amount):
-        if self.total_available > 0:
-            return amount #??????????????????
+        if amount < self.total_available.kwota:
+            self.stan -= amount
+            return Amount(amount, self.skrot)
+        else:
+            print("Sorry, amount not available")
 
     def pay_in(self, amount):
-        self.stan_konta += amount
+        self.stan += amount
+
+ca1 = CurrencyAccount('PLN',1000,1000)
+print(ca1.total_available)
+ca1.pay_out(500)
+print(ca1.total_available)
+ca1.pay_in(1000)
+print(ca1.total_available)
+
 
 @dataclass
 class ExchangeOffice:
-    wartosc_spreadu: float
-    dostepne_waluty: ['EUR','PLN','USD','CHF']
+    spread: float = 0.05
+    waluty: tuple = (c1,c2,c3,c4)
 
-    def _exchange_to_pln(self, amount):
-        pass
+    def _get_currency(self, abbr):
+        return [currency for currency in self.waluty if currency.skrot == abbr][0]
+        # for currency in self.waluty:
+        #     if currency.skrot == abbr:
+        #         return currency
 
-    def _exchange_from_pln(amount, target):
-        pass
+    def exchange_to_pln(self, amount): #wymien ze złotówek, amount - to krotka nazwa
+        currency = self._get_currency(amount.skrot)
+        return Amount( (currency.przelicznik- self.spread/2)*amount.kwota, 'PLN') #uwzględniamy spread
+
+
+
+    def exchange_from_pln(self, amount, target):
+        currency = self._get_currency(target)
+        return Amount( 1/(currency.przelicznik + self.spread / 2) * amount.kwota, target)  # uwzględniamy spread
+
+
+
+eo1 = ExchangeOffice()
+kwota_wplaty = Amount(1000,'USD')
+wyplata1 = eo1.exchange_to_pln(kwota_wplaty)
+print(wyplata1)
+kwota_wplaty = Amount(1000, 'PLN')
+wyplata2 = eo1.exchange_from_pln(kwota_wplaty, "USD")
+print(wyplata2)
