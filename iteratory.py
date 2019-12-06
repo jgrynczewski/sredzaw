63ac126bc1e0bc144ef309caed205cf1359a1cff
Magda <magda.bochinska@gmail.com>
2019-12-06 11:04:49 +0100

    interpreter

--- iteratory.py
+++ iteratory.py
@@ -3,3 +3,37 @@
 my_list = [1,2,3,4]
 for item in my_list:
     print(item)
+
+for item in 'asd"':
+    print(item)
+
+alist= [1,2,3 ,4]
+my_iterator  = iter(alist)
+while True:
+    try:
+        item = (next(my_iterator))
+        print(item)
+    except StopIteration:
+        break
+
+
+class MyCounter:
+    def __init__(self, low, high):
+        self.current = low
+        self.high = high
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current >= self.high:
+            raise StopIteration
+        else:
+            self.current += 1
+            return self.current -1
+
+my_iterator_2 = MyCounter(5,100)
+print(list(my_iterator_2))
+
+for item in my_iterator_2:
+    print(list(item))
